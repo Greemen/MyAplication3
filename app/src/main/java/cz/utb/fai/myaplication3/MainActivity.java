f611b9474703ba0c3f4b82e9a2c009a540fa49a1
Greemen <j_andrysek@utb.cz>
2023-01-08 15:11:34 +0100

    Layout reorganized

--- app/src/main/java/cz/utb/fai/myaplication3/MainActivity.java
+++ app/src/main/java/cz/utb/fai/myaplication3/MainActivity.java
@@ -1,24 +1,23 @@
 package cz.utb.fai.myaplication3;
 
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
-import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-
-import cz.utb.fai.myaplication3.ui.main.SectionsPagerAdapter;
 import cz.utb.fai.myaplication3.databinding.ActivityMainBinding;
+import cz.utb.fai.myaplication3.ui.main.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +40,6 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
     }
 }
