0b5bf4933ce9e0d32b1a6d9b6e02529dd7eeea49
Greemen <j_andrysek@utb.cz>
2023-01-18 13:06:25 +0100

    splash screen

--- app/src/main/java/cz/utb/fai/myaplication3/MainActivity.java
+++ app/src/main/java/cz/utb/fai/myaplication3/MainActivity.java
@@ -2,6 +2,7 @@ package cz.utb.fai.myaplication3;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -23,8 +24,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+
+
 
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = binding.viewPager;
